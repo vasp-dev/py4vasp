38fab685c2dc5c3fc98dac6f08d33cf28ced33a6
Martin Schlipf <martin.schlipf@gmail.com>
2020-01-08 14:35:37 +0000

    Implement plot and reading functionality for projected DOS
    Add a selection mechanism that allows to specify the atoms and/or orbitals for the projected DOS.
    Make returning a dictionary the default for read and the dataframe a separate function to_frame.

--- src/py4vasp/data/dos.py
+++ src/py4vasp/data/dos.py
@@ -1,30 +1,78 @@
+import re
+import functools
+import itertools
+import numpy as np
 import pandas as pd
 import cufflinks as cf
+from collections import namedtuple
 
 cf.go_offline()
 
 
 class Dos:
+    _Index = namedtuple("_Index", "spin, atom, orbital")
+    _Atom = namedtuple("_Atom", "indices, label")
+    _Orbital = namedtuple("_Orbital", "indices, label")
+    _Spin = namedtuple("_Spin", "indices, label")
+
     def __init__(self, vaspout):
         self._fermi_energy = vaspout["results/dos/efermi"][()]
         self._energies = vaspout["results/dos/energies"]
         self._dos = vaspout["results/dos/dos"]
         self._spin_polarized = self._dos.shape[0] == 2
+        self._has_partial_dos = vaspout["results/dos/jobpar"][()] == 1
+        if self._has_partial_dos:
+            self._init_partial_dos(vaspout)
 
-    def read(self):
-        if self._spin_polarized:
-            data = {"up": self._dos[0, :], "down": self._dos[1, :]}
-        else:
-            data = {"total": self._dos[0, :]}
-        data["energies"] = self._energies[:] - self._fermi_energy
-        df = pd.DataFrame(data)
-        df.fermi_energy = self._fermi_energy
-        return df
+    def _init_partial_dos(self, vaspout):
+        self._partial_dos = vaspout["results/dos/dospar"]
+        ion_types = vaspout["results/positions/ion_types"]
+        ion_types = [type.decode().strip() for type in ion_types]
+        self._init_atom_dict(ion_types, vaspout["results/positions/number_ion_types"])
+        orbitals = vaspout["results/projectors/lchar"]
+        orbitals = [orb.decode().strip() for orb in orbitals]
+        self._init_orbital_dict(orbitals)
+        self._init_spin_dict()
 
-    def plot(self):
-        df = self.read()
+    def _init_atom_dict(self, ion_types, number_ion_types):
+        num_atoms = self._partial_dos.shape[1]
+        all_atoms = self._Atom(indices=range(num_atoms), label=None)
+        self._atom_dict = {"*": all_atoms}
+        start = 0
+        for type, number in zip(ion_types, number_ion_types):
+            _range = range(start, start + number)
+            self._atom_dict[type] = self._Atom(indices=_range, label=type)
+            for i in _range:
+                # create labels like Si_1, Si_2, Si_3 (starting at 1)
+                label = type + "_" + str(_range.index(i) + 1)
+                self._atom_dict[str(i + 1)] = self._Atom(indices=[i], label=label)
+            start += number
+        # atoms may be preceeded by :
+        for key in self._atom_dict.copy():
+            self._atom_dict[key + ":"] = self._atom_dict[key]
+
+    def _init_orbital_dict(self, orbitals):
+        num_orbitals = self._partial_dos.shape[2]
+        all_orbitals = self._Orbital(indices=range(num_orbitals), label=None)
+        self._orbital_dict = {"*": all_orbitals}
+        for i, orbital in enumerate(orbitals):
+            self._orbital_dict[orbital] = self._Orbital(indices=[i], label=orbital)
+        if "px" in self._orbital_dict:
+            self._orbital_dict["p"] = self._Orbital(indices=range(1, 4), label="p")
+            self._orbital_dict["d"] = self._Orbital(indices=range(4, 9), label="d")
+            self._orbital_dict["f"] = self._Orbital(indices=range(9, 16), label="f")
+
+    def _init_spin_dict(self):
+        labels = ["up", "down"] if self._spin_polarized else [None]
+        self._spin_dict = {
+            key: self._Spin(indices=[i], label=key) for i, key in enumerate(labels)
+        }
+
+    def plot(self, selection=None):
+        df = self.to_frame(selection)
         if self._spin_polarized:
-            df.down = -df.down
+            for col in filter(lambda col: "down" in col, df):
+                df[col] = -df[col]
         default = {
             "x": "energies",
             "xTitle": "Energy (eV)",
@@ -32,3 +80,72 @@ class Dos:
             "asFigure": True,
         }
         return df.iplot(**default)
+
+    def read(self, selection=None):
+        return self.to_dict(selection)
+
+    def to_dict(self, selection=None):
+        return {**self._read_data(selection), "fermi_energy": self._fermi_energy}
+
+    def to_frame(self, selection=None):
+        df = pd.DataFrame(self._read_data(selection))
+        df.fermi_energy = self._fermi_energy
+        return df
+
+    def _read_data(self, selection):
+        return {
+            **self._read_energies(),
+            **self._read_total_dos(),
+            **self._read_partial_dos(selection),
+        }
+
+    def _read_energies(self):
+        return {"energies": self._energies[:] - self._fermi_energy}
+
+    def _read_total_dos(self):
+        if self._spin_polarized:
+            return {"up": self._dos[0, :], "down": self._dos[1, :]}
+        else:
+            return {"total": self._dos[0, :]}
+
+    def _read_partial_dos(self, selection):
+        if selection is None:
+            return {}
+        self._raise_error_if_partial_Dos_not_available()
+        parts = self._parse_filter(selection)
+        return self._read_elements(parts)
+
+    def _raise_error_if_partial_Dos_not_available(self):
+        if not self._has_partial_dos:
+            raise ValueError(
+                "Filtering requires partial DOS which was not found in HDF5 file."
+            )
+
+    def _parse_filter(self, selection):
+        atom = self._atom_dict["*"]
+        selection = re.sub("\s*:\s*", ": ", selection)
+        for part in re.split("[ ,]+", selection):
+            if part in self._orbital_dict:
+                orbital = self._orbital_dict[part]
+            else:
+                atom = self._atom_dict[part]
+                orbital = self._orbital_dict["*"]
+            if ":" not in part:  # exclude ":" because it starts a new atom
+                for spin in self._spin_dict.values():
+                    yield atom, orbital, spin
+
+    def _read_elements(self, parts):
+        res = {}
+        for atom, orbital, spin in parts:
+            label = self._merge_labels([atom.label, orbital.label, spin.label])
+            index = self._Index(spin.indices, atom.indices, orbital.indices)
+            res[label] = self._read_element(index)
+        return res
+
+    def _merge_labels(self, labels):
+        return "_".join(filter(None, labels))
+
+    def _read_element(self, index):
+        sum_dos = lambda dos, i: dos + self._partial_dos[i]
+        zero_dos = np.zeros_like(self._energies[:])
+        return functools.reduce(sum_dos, itertools.product(*index), zero_dos)
--- tests/data/test_dos.py
+++ tests/data/test_dos.py
@@ -2,68 +2,209 @@ from py4vasp.data import Dos
 import pytest
 import h5py
 import numpy as np
+from numpy.testing import assert_array_almost_equal_nulp
 from tempfile import TemporaryFile
+
+
+def assert_allclose(actual, desired):
+    assert_array_almost_equal_nulp(actual, desired, 10)
 
 
 @pytest.fixture
 def nonmagnetic_Dos():
     """ Setup a nonmagnetic Dos file containing all important quantities."""
-    tf = TemporaryFile()
-    h5f = h5py.File(tf, "a")
-    energies = np.linspace(-1, 3)
-    h5f["results/dos/energies"] = energies
-    h5f["results/dos/dos"] = np.array([energies ** 2])
-    h5f["results/dos/efermi"] = 1.372
-    return h5f
+    return _nonmagnetic_Dos()
+
+
+def _nonmagnetic_Dos():
+    ref = {"energies": np.linspace(-1, 3), "fermi_energy": 1.372}
+    ref["dos"] = ref["energies"] ** 2
+    h5f = h5py.File(TemporaryFile(), "a")
+    h5f["results/dos/energies"] = ref["energies"]
+    h5f["results/dos/dos"] = np.array([ref["dos"]])
+    h5f["results/dos/efermi"] = ref["fermi_energy"]
+    h5f["results/dos/jobpar"] = 0
+    return h5f, ref
 
 
 def test_nonmagnetic_Dos_read(nonmagnetic_Dos):
+    h5f, ref = nonmagnetic_Dos
+    dos = Dos(h5f).read()
+    assert_allclose(dos["energies"], ref["energies"] - ref["fermi_energy"])
+    assert_allclose(dos["total"], ref["dos"])
+    assert dos["fermi_energy"] == ref["fermi_energy"]
+
+
+def test_nonmagnetic_Dos_read_error(nonmagnetic_Dos):
+    h5f, _ = nonmagnetic_Dos
+    with pytest.raises(ValueError):
+        Dos(h5f).read("s")
+
+
+def test_nonmagnetic_Dos_to_frame(nonmagnetic_Dos):
     """ Test whether reading the nonmagnetic Dos yields the expected results."""
-    dos = Dos(nonmagnetic_Dos).read()
-    fermi_energy = nonmagnetic_Dos["results/dos/efermi"][()]
-    energies = nonmagnetic_Dos["results/dos/energies"][:] - fermi_energy
-    assert np.all(dos.energies == energies)
-    assert np.all(dos.total == nonmagnetic_Dos["results/dos/dos"][0, :])
-    assert dos.fermi_energy == fermi_energy
+    h5f, ref = nonmagnetic_Dos
+    dos = Dos(h5f).to_frame()
+    assert_allclose(dos.energies, ref["energies"] - ref["fermi_energy"])
+    assert_allclose(dos.total, ref["dos"])
+    assert dos.fermi_energy == ref["fermi_energy"]
 
 
 def test_nonmagnetic_Dos_plot(nonmagnetic_Dos):
     """ Test whether plotting the nonmagnetic Dos yields the expected results."""
-    fig = Dos(nonmagnetic_Dos).plot()
-    fermi_energy = nonmagnetic_Dos["results/dos/efermi"][()]
-    energies = nonmagnetic_Dos["results/dos/energies"][:] - fermi_energy
+    h5f, ref = nonmagnetic_Dos
+    fig = Dos(h5f).plot()
     assert fig.layout.xaxis.title.text == "Energy (eV)"
     assert fig.layout.yaxis.title.text == "DOS (1/eV)"
-    assert np.all(fig.data[0].x == energies)
-    assert np.all(fig.data[0].y == nonmagnetic_Dos["results/dos/dos"][0, :])
+    assert len(fig.data) == 1
+    assert_allclose(fig.data[0].x, ref["energies"] - ref["fermi_energy"])
+    assert_allclose(fig.data[0].y, ref["dos"])
 
 
 @pytest.fixture
 def magnetic_Dos():
+    return _magnetic_Dos()
+
+
+def _magnetic_Dos():
     """ Setup a magnetic Dos file containing all relevant quantities."""
-    tf = TemporaryFile()
-    h5f = h5py.File(tf, "a")
-    energies = np.linspace(-2, 2)
-    h5f["results/dos/energies"] = energies
-    h5f["results/dos/dos"] = np.array([(energies + 0.5) ** 2, (energies - 0.5) ** 2])
+    ref = {"energies": np.linspace(-2, 2), "fermi_energy": -0.137}
+    ref["up"] = (ref["energies"] + 0.5) ** 2
+    ref["down"] = (ref["energies"] - 0.5) ** 2
+    h5f = h5py.File(TemporaryFile(), "a")
+    h5f["results/dos/energies"] = ref["energies"]
+    h5f["results/dos/dos"] = np.array([ref["up"], ref["down"]])
     h5f["results/dos/efermi"] = -0.137
-    return h5f
+    h5f["results/dos/jobpar"] = 0
+    return h5f, ref
 
 
-def test_magnetic_Dos_read(magnetic_Dos):
-    """ Test whether plotting the magnetic Dos yields the expected results."""
-    dos = Dos(magnetic_Dos).read()
-    fermi_energy = magnetic_Dos["results/dos/efermi"][()]
-    energies = magnetic_Dos["results/dos/energies"][:] - fermi_energy
-    assert np.all(dos.energies == energies)
-    assert np.all(dos.up == magnetic_Dos["results/dos/dos"][0, :])
-    assert np.all(dos.down == magnetic_Dos["results/dos/dos"][1, :])
-    assert dos.fermi_energy == fermi_energy
+def test_magnetic_Dos_to_frame(magnetic_Dos):
+    """ Test whether reading the magnetic Dos yields the expected results."""
+    h5f, ref = magnetic_Dos
+    dos = Dos(h5f).to_frame()
+    assert_allclose(dos.energies, ref["energies"] - ref["fermi_energy"])
+    assert_allclose(dos.up, ref["up"])
+    assert_allclose(dos.down, ref["down"])
+    assert dos.fermi_energy == ref["fermi_energy"]
 
 
 def test_magnetic_Dos_plot(magnetic_Dos):
     """ Test whether plotting the magnetic Dos yields the expected results."""
-    fig = Dos(magnetic_Dos).plot()
-    assert np.all(fig.data[0].x == fig.data[1].x)
-    assert np.all(fig.data[0].y == magnetic_Dos["results/dos/dos"][0, :])
-    assert np.all(fig.data[1].y == -magnetic_Dos["results/dos/dos"][1, :])
+    h5f, ref = magnetic_Dos
+    fig = Dos(h5f).plot()
+    assert len(fig.data) == 2
+    assert_allclose(fig.data[0].x, fig.data[1].x)
+    assert_allclose(fig.data[0].y, ref["up"])
+    assert_allclose(fig.data[1].y, -ref["down"])
+
+
+@pytest.fixture
+def nonmagnetic_l_Dos():
+    """ Setup a l resolved Dos file containing all relevant quantities."""
+    h5f, ref = _nonmagnetic_Dos()
+    ref["Si_s"] = np.sqrt(np.abs(ref["energies"]))
+    ref["Si_p"] = ref["energies"] + 0.5
+    ref["Si_d"] = ref["energies"] ** 2
+    ref["C1_s"] = np.abs(ref["energies"])
+    ref["C1_p"] = (1 - ref["energies"]) ** 2
+    ref["C2_s"] = np.sqrt(np.abs(ref["energies"]) + 1)
+    ref["C2_p"] = 1 - ref["energies"] ** 2
+    num_spins = 1
+    atoms = ["Si", "C1", "C2"]
+    lmax = 4
+    num_energy = len(ref["energies"])
+    h5f["results/dos/jobpar"][()] = 1
+    h5f["results/projectors/lchar"] = np.array([" s", " p", " d", " f"], dtype="S")
+    h5f["results/positions/ion_types"] = np.array(["Si", "C "], dtype="S")
+    h5f["results/positions/number_ion_types"] = [1, 2]
+    h5f["results/dos/dospar"] = np.zeros((num_spins, len(atoms), lmax, num_energy))
+    orbitals = ["s", "p", "d"]
+    for iatom, atom in enumerate(atoms):
+        for l, orbital in enumerate(orbitals):
+            key = atom + "_" + orbital
+            if key in ref:
+                h5f["results/dos/dospar"][:, iatom, l] = ref[key]
+    return h5f, ref
+
+
+def test_nonmagnetic_l_Dos_to_frame(nonmagnetic_l_Dos):
+    """ Test whether reading the nonmagnetic l resolved Dos yields the expected results."""
+    h5f, ref = nonmagnetic_l_Dos
+    equivalent_selections = [
+        "s Si:d Si C:s,p 1:p 2 3:s",
+        "1: p, C : s Si : d, *: s, 2 Si:* C: p 3 : s",
+    ]
+    for selection in equivalent_selections:
+        dos = Dos(h5f).to_frame(selection)
+        assert_allclose(dos.s, ref["Si_s"] + ref["C1_s"] + ref["C2_s"])
+        assert_allclose(dos.Si, ref["Si_s"] + ref["Si_p"] + ref["Si_d"])
+        assert_allclose(dos.Si_d, ref["Si_d"])
+        assert_allclose(dos.Si_1_p, ref["Si_p"])
+        assert_allclose(dos.C_s, ref["C1_s"] + ref["C2_s"])
+        assert_allclose(dos.C_p, ref["C1_p"] + ref["C2_p"])
+        assert_allclose(dos.C_1, ref["C1_s"] + ref["C1_p"])
+        assert_allclose(dos.C_2_s, ref["C2_s"])
+
+
+def test_nonmagnetic_l_Dos_plot(nonmagnetic_l_Dos):
+    """ Test whether plotting the nonmagnetic l resolved Dos yields the expected results."""
+    h5f, ref = nonmagnetic_l_Dos
+    selection = "p 3 Si:d"
+    fig = Dos(h5f).plot(selection)
+    assert len(fig.data) == 4  # total Dos + 3 selections
+    assert_allclose(fig.data[1].y, ref["Si_p"] + ref["C1_p"] + ref["C2_p"])
+    assert_allclose(fig.data[2].y, ref["C2_s"] + ref["C2_p"])
+    assert_allclose(fig.data[3].y, ref["Si_d"])
+
+
+@pytest.fixture
+def magnetic_lm_Dos():
+    """ Setup a lm resolved Dos file containing all relevant quantities."""
+    h5f, ref = _magnetic_Dos()
+    num_spins = 2
+    lm_size = 16
+    num_energy = len(ref["energies"])
+    sp_orbitals = ["    s", "   py", "   pz", "   px"]
+    d_orbitals = ["  dxy", "  dyz", "  dz2", "  dxz", "x2-y2"]
+    f_orbitals = ["fy3x2", " fxyz", " fyz2", "  fz3", " fxz2", " fzx2", "  fx3"]
+    orbitals = sp_orbitals + d_orbitals + f_orbitals
+    h5f["results/dos/jobpar"][()] = 1
+    h5f["results/projectors/lchar"] = np.array(orbitals, dtype="S")
+    h5f["results/positions/ion_types"] = np.array(["Fe"], dtype="S")
+    h5f["results/positions/number_ion_types"] = [1]
+    h5f["results/dos/dospar"] = np.zeros((num_spins, 1, lm_size, num_energy))
+    for ispin, spin in enumerate(["up", "down"]):
+        for lm, orbital in enumerate(orbitals):
+            key = orbital.strip() + "_" + spin
+            ref[key] = np.random.random(num_energy)
+            h5f["results/dos/dospar"][ispin, :, lm] = ref[key]
+    return h5f, ref
+
+
+def test_magnetic_lm_Dos_read(magnetic_lm_Dos):
+    """ Test whether reading lm resolved Dos works as expected."""
+    h5f, ref = magnetic_lm_Dos
+    dos = Dos(h5f).read("px p d f")
+    assert_allclose(dos["px_up"], ref["px_up"])
+    assert_allclose(dos["px_down"], ref["px_down"])
+    assert_allclose(dos["p_up"], ref["px_up"] + ref["py_up"] + ref["pz_up"])
+    d_down = ref["dxy_down"] + ref["dyz_down"] + ref["dz2_down"]
+    d_down += ref["dxz_down"] + ref["x2-y2_down"]
+    assert_allclose(dos["d_down"], d_down)
+    f_up = ref["fy3x2_up"] + ref["fxyz_up"] + ref["fyz2_up"] + ref["fz3_up"]
+    f_up += ref["fxz2_up"] + ref["fzx2_up"] + ref["fx3_up"]
+    assert_allclose(dos["f_up"], f_up)
+
+
+def test_magnetic_lm_Dos_plot(magnetic_lm_Dos):
+    """ Test whether plotting lm resolved Dos works as expected."""
+    h5f, ref = magnetic_lm_Dos
+    fig = Dos(h5f).plot("dxz p")
+    data = fig.data
+    assert len(data) == 6  # spin resolved total + 2 selections
+    names = [d.name for d in data]
+    dxz_up = names.index("dxz_up")
+    assert_allclose(data[dxz_up].y, ref["dxz_up"])
+    p_down = names.index("p_down")
+    assert_allclose(data[p_down].y, -(ref["px_down"] + ref["py_down"] + ref["pz_down"]))
