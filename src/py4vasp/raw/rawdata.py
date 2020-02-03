b7855c999c03c1d4e1080f3a2e99aae33af51c92
Martin Schlipf <martin.schlipf@gmail.com>
2020-02-03 13:35:09 +0000

    Resolve the missing kpoint labels issue
    Make the default in the raw type None so that the test require explicit treatment of these cases.

--- src/py4vasp/raw/rawdata.py
+++ src/py4vasp/raw/rawdata.py
@@ -52,8 +52,8 @@ class Band:
     kpoints: np.ndarray
     eigenvalues: np.ndarray
     cell: Cell
-    labels: np.ndarray = np.empty(0, dtype="S")
-    label_indices: np.ndarray = np.empty(0, dtype="int")
+    labels: np.ndarray = None
+    label_indices: np.ndarray = None
     projections: np.ndarray = None
     projectors: Projectors = None
     __eq__ = _dataclass_equal
