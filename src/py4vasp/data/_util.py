c14f0ec0cc4cee1b7c05a5ff4719caff47c2edec
Martin Schlipf <martin.schlipf@gmail.com>
2020-02-18 15:51:03 +0100

    Refactor: use nullcontext
    This avoids having a duplicate getattr statement

--- src/py4vasp/data/_util.py
+++ src/py4vasp/data/_util.py
@@ -1,11 +1,12 @@
-from contextlib import contextmanager
+from contextlib import contextmanager, nullcontext
 import py4vasp.raw as raw
 
 
 @contextmanager
 def from_file(cls, file, attr):
     if file is None or isinstance(file, str):
-        with raw.File(file) as local_file:
-            yield cls(getattr(local_file, attr)())
+        context = raw.File(file)
     else:
+        context = nullcontext(file)
+    with context as file:
         yield cls(getattr(file, attr)())
