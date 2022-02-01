052fcb56f83f492d07cccea94a6654d2a749154a
Martin Schlipf <martin.schlipf@gmail.com>
2022-02-01 09:48:04 +0100

    Bump version to indicate prerelease

--- src/py4vasp/__init__.py
+++ src/py4vasp/__init__.py
@@ -1,5 +1,5 @@
 from .calculation import Calculation
 from py4vasp._third_party.interactive import set_error_handling
 
-__version__ = "0.3.1"
+__version__ = "0.4.0-alpha.0"
 set_error_handling("Minimal")
