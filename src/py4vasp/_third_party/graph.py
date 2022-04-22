be08c0755a6dd3781268564ab9f42fb3e9ca7dc3
Martin Schlipf <martin.schlipf@gmail.com>
2022-04-22 10:04:39 +0200

    Refactor the functions to methods

--- src/py4vasp/_third_party/graph.py
+++ src/py4vasp/_third_party/graph.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, replace
 import itertools
 import numpy as np
-from typing import Sequence
+from typing import Sequence, NamedTuple
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
@@ -15,9 +15,49 @@ class Series:
     name: str
     width: np.ndarray = None
     y2: bool = False
+    color: str = None
 
-    def to_plotly(self):
-        return go.Scatter(x=self.x, y=self.y, name=self.name)
+    def _generate_traces(self):
+        first_trace = True
+        for y in np.atleast_2d(self.y.T):
+            yield self._make_trace(y, first_trace)
+            first_trace = False
+
+    def _make_trace(self, y, first_trace):
+        if self.width is None:
+            options = self._options_line(y, first_trace)
+        else:
+            options = self._options_area(y, first_trace)
+        return go.Scatter(**options)
+
+    def _options_line(self, y, first_trace):
+        return {
+            **self._common_options(first_trace),
+            "x": self.x,
+            "y": y,
+            "line": {"color": self.color},
+        }
+
+    def _options_area(self, y, first_trace):
+        upper = y + self.width
+        lower = y - self.width
+        return {
+            **self._common_options(first_trace),
+            "x": np.concatenate((self.x, self.x[::-1])),
+            "y": np.concatenate((lower, upper[::-1])),
+            "mode": "none",
+            "fill": "toself",
+            "fillcolor": self.color,
+            "opacity": 0.5,
+        }
+
+    def _common_options(self, first_trace):
+        return {
+            "name": self.name,
+            "legendgroup": self.name,
+            "showlegend": first_trace,
+            "yaxis": "y2" if self.y2 else "y",
+        }
 
 
 @dataclass
@@ -30,90 +70,42 @@ class Graph:
     title: str = None
 
     def to_plotly(self):
-        figure = _make_plotly_figure(self)
-        for trace in _generate_plotly_traces(self.series):
+        figure = self._make_plotly_figure()
+        for trace in self._generate_plotly_traces():
             figure.add_trace(trace)
         return figure
 
     def _ipython_display_(self):
         self.to_plotly()._ipython_display_()
 
+    def _generate_plotly_traces(self):
+        colors = itertools.cycle(_vasp_colors)
+        for series in np.atleast_1d(self.series):
+            if not series.color:
+                series = replace(series, color=next(colors))
+            yield from series._generate_traces()
 
-def _make_plotly_figure(graph):
-    figure = _figure_with_one_or_two_y_axes(graph)
-    _set_xaxis_options(graph, figure)
-    _set_yaxis_options(graph, figure)
-    figure.layout.title.text = graph.title
-    return figure
+    def _make_plotly_figure(self):
+        figure = self._figure_with_one_or_two_y_axes()
+        self._set_xaxis_options(figure)
+        self._set_yaxis_options(figure)
+        figure.layout.title.text = self.title
+        return figure
 
-
-def _figure_with_one_or_two_y_axes(graph):
-    if any(series.y2 for series in np.atleast_1d(graph.series)):
-        return make_subplots(specs=[[{"secondary_y": True}]])
-    else:
-        return go.Figure()
-
-
-def _set_xaxis_options(graph, figure):
-    figure.layout.xaxis.title.text = graph.xlabel
-    if graph.xticks:
-        figure.layout.xaxis.tickmode = "array"
-        figure.layout.xaxis.tickvals = tuple(graph.xticks.keys())
-        figure.layout.xaxis.ticktext = tuple(graph.xticks.values())
-
-
-def _set_yaxis_options(graph, figure):
-    figure.layout.yaxis.title.text = graph.ylabel
-    if graph.y2label:
-        figure.layout.yaxis2.title.text = graph.y2label
-
-
-def _generate_plotly_traces(series):
-    colors = itertools.cycle(_vasp_colors)
-    for series in np.atleast_1d(series):
-        factory = _PlotlyTraceFactory(color=next(colors))
-        for y in np.atleast_2d(series.y.T):
-            yield factory.make_trace(replace(series, y=y))
-            factory.first_trace = False
-
-
-@dataclass
-class _PlotlyTraceFactory:
-    color: str
-    first_trace: bool = True
-
-    def make_trace(self, series):
-        if series.width is None:
-            options = self._options_line(series)
+    def _figure_with_one_or_two_y_axes(self):
+        if any(series.y2 for series in np.atleast_1d(self.series)):
+            return make_subplots(specs=[[{"secondary_y": True}]])
         else:
-            options = self._options_area(series)
-        return go.Scatter(**options)
+            return go.Figure()
 
-    def _options_line(self, series):
-        return {
-            **self._common_options(series),
-            "x": series.x,
-            "y": series.y,
-            "line": {"color": self.color},
-        }
+    def _set_xaxis_options(self, figure):
+        figure.layout.xaxis.title.text = self.xlabel
+        if self.xticks:
+            figure.layout.xaxis.tickmode = "array"
+            figure.layout.xaxis.tickvals = tuple(self.xticks.keys())
+            figure.layout.xaxis.ticktext = tuple(self.xticks.values())
 
-    def _options_area(self, series):
-        upper = series.y + series.width
-        lower = series.y - series.width
-        return {
-            **self._common_options(series),
-            "x": np.concatenate((series.x, series.x[::-1])),
-            "y": np.concatenate((lower, upper[::-1])),
-            "mode": "none",
-            "fill": "toself",
-            "fillcolor": self.color,
-            "opacity": 0.5,
-        }
-
-    def _common_options(self, series):
-        return {
-            "name": series.name,
-            "legendgroup": series.name,
-            "showlegend": self.first_trace,
-            "yaxis": "y2" if series.y2 else "y",
-        }
+    def _set_yaxis_options(self, figure):
+        figure.layout.yaxis.title.text = self.ylabel
+        if self.y2label:
+            figure.layout.yaxis2.title.text = self.y2label
