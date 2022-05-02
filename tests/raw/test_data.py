4a544b1e9f59a4b3d2df083ddef732937ca46ff0
Martin Schlipf <martin.schlipf@gmail.com>
2022-05-02 16:57:55 +0200

    Add a few more tests

--- tests/raw/test_data.py
+++ tests/raw/test_data.py
@@ -1,5 +1,5 @@
 from py4vasp.raw import VaspData
-from hypothesis import given
+from hypothesis import given, assume
 import hypothesis.strategies as strategy
 from hypothesis.extra.numpy import mutually_broadcastable_shapes, arrays
 import numpy as np
@@ -37,3 +37,34 @@ def test_operators(ops, Assert):
         assert np.all((vasp_x < vasp_y) == (ref_x < ref_y))
         assert np.all((vasp_x >= vasp_y) == (ref_x >= ref_y))
         assert np.all((vasp_x <= vasp_y) == (ref_x <= ref_y))
+        assume(np.all(ref_y) != 0)
+        Assert.allclose(vasp_x / vasp_y, ref_x / ref_y)
+        Assert.allclose(vasp_x % vasp_y, ref_x % ref_y)
+        Assert.allclose(vasp_x // vasp_y, ref_x // ref_y)
+    # make power operator work
+    ref_x = np.abs(ref_x)
+    ref_y = np.log(np.abs(ref_y))
+    vasp_x, vasp_y = VaspData(ref_x), VaspData(ref_y)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        Assert.allclose(vasp_x ** vasp_y, ref_x ** ref_y)
+
+
+@given(ops=operands())
+def test_functions(ops, Assert):
+    ref_x, ref_y = ops
+    vasp_x, vasp_y = VaspData(ref_x), VaspData(ref_y)
+    with warnings.catch_warnings():
+        # NaN of infinite may be tested, so we ignore the warnings
+        warnings.simplefilter("ignore")
+        Assert.allclose(np.sin(vasp_x), np.sin(ref_x))
+        Assert.allclose(np.cos(vasp_x), np.cos(ref_x))
+        Assert.allclose(np.tan(vasp_x), np.tan(ref_x))
+        Assert.allclose(np.arcsin(vasp_x), np.arcsin(ref_x))
+        Assert.allclose(np.arccos(vasp_x), np.arccos(ref_x))
+        Assert.allclose(np.arctan(vasp_x), np.arctan(ref_x))
+        Assert.allclose(np.abs(vasp_x), np.abs(ref_x))
+        Assert.allclose(np.linalg.norm(vasp_x), np.linalg.norm(ref_x))
+        Assert.allclose(np.outer(vasp_x, vasp_y), np.outer(ref_x, ref_y))
+        Assert.allclose(np.maximum(vasp_x, vasp_y), np.maximum(ref_x, ref_y))
+        Assert.allclose(np.minimum(vasp_x, vasp_y), np.minimum(ref_x, ref_y))
