e7ee5ae6025cd4fa7a5d7425a7390a5eca7bdcac
Martin Schlipf <martin.schlipf@gmail.com>
2022-05-03 09:56:57 +0200

    Update documentation

--- src/py4vasp/raw/data.py
+++ src/py4vasp/raw/data.py
@@ -2,6 +2,25 @@ import numpy as np
 
 
 class VaspData(np.lib.mixins.NDArrayOperatorsMixin):
+    """Wraps the data produced by the VASP calculation.
+
+    Instead of exposing the underlying file structure directly, the data is wrapped in
+    this container. This allows changing the way the data is internally represented
+    without affecting the user. In particular, the data is possibly only lazily loaded
+    when it is actually necessary.
+
+    By inheriting from NDArrayOperatorsMixin most numpy functionality except for the
+    class attributes should work. If any other feature is needed any instance of this
+    class can be passed into a numpy array. Please be aware that using the data in
+    this way will access the file. If performance is an issue, make sure that this
+    file I/O is reduced as much as possible.
+
+    Parameters
+    ----------
+    data
+        The data wrapped by this container.
+    """
+
     def __init__(self, data):
         self._data = data
 
@@ -13,16 +32,20 @@ class VaspData(np.lib.mixins.NDArrayOperatorsMixin):
 
     @property
     def ndim(self):
+        "The number of dimensions of the data."
         return self._data.ndim
 
     @property
     def size(self):
+        "The total number of elements of the data."
         return self._data.size
 
     @property
     def shape(self):
+        "The shape of the data. Empty tuple for scalar data."
         return self._data.shape
 
     @property
     def dtype(self):
+        "Describes the type of the contained data."
         return self._data.dtype
