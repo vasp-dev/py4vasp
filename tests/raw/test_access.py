e9f3113ff1d693536e759991164dab11be1e8a19
Martin Schlipf <martin.schlipf@gmail.com>
2022-05-04 13:40:24 +0200

    Refactor tests

--- tests/raw/test_access.py
+++ tests/raw/test_access.py
@@ -26,7 +26,7 @@ def test_access_quantity(mock_access):
     mock_file, sources = mock_access
     source = sources[quantity]["default"]
     with raw.access(quantity) as opt_arg:
-        check_file_access(mock_file, source, DEFAULT_FILE)
+        check_single_file_access(mock_file, DEFAULT_FILE, source)
         assert opt_arg.mandatory == mock_read_result(source.data.mandatory)
         assert opt_arg.optional == mock_read_result(source.data.optional)
 
@@ -36,7 +36,7 @@ def test_access_other_file(mock_access):
     mock_file, sources = mock_access
     source = sources[quantity]["default"]
     with raw.access(quantity) as simple:
-        check_file_access(mock_file, source, source.file)
+        check_single_file_access(mock_file, source.file, source)
         assert simple.foo == mock_read_result(source.data.foo)
         assert simple.bar == mock_read_result(source.data.bar)
 
@@ -46,15 +46,9 @@ def test_access_optional_argument(mock_access):
     mock_file, sources = mock_access
     source = sources[quantity]["mandatory"]
     with raw.access(quantity, source="mandatory") as opt_arg:
-        check_file_access(mock_file, source, DEFAULT_FILE)
+        check_single_file_access(mock_file, DEFAULT_FILE, source)
         assert opt_arg.mandatory == mock_read_result(source.data.mandatory)
         assert opt_arg.optional == None
-
-
-def check_file_access(mock_file, source, filename):
-    mock_file.assert_called_once_with(filename, "r")
-    h5f = mock_file.return_value.__enter__.return_value
-    h5f.get.assert_has_calls(expected_calls(source), any_order=True)
 
 
 def test_access_with_link(mock_access):
@@ -63,13 +57,21 @@ def test_access_with_link(mock_access):
     mock_file, sources = mock_access
     source = sources[quantity]["default"]
     with raw.access(quantity) as with_link:
-        file_calls.append(call(DEFAULT_FILE, "r"))
-        mock_file.assert_has_calls(file_calls, any_order=True)
+        file_calls += [call(DEFAULT_FILE, "r")]
         get_calls += list(expected_calls(source))
-        h5f = mock_file.return_value.__enter__.return_value
-        h5f.get.assert_has_calls(expected_calls(source), any_order=True)
+        check_file_access(mock_file, file_calls, get_calls)
         assert with_link.baz == mock_read_result(source.data.baz)
         assert with_link.simple == reference
+
+
+def check_single_file_access(mock_file, filename, source):
+    check_file_access(mock_file, (call(filename, "r"),), expected_calls(source))
+
+
+def check_file_access(mock_file, file_calls, get_calls):
+    mock_file.assert_has_calls(file_calls, any_order=True)
+    h5f = mock_file.return_value.__enter__.return_value
+    h5f.get.assert_has_calls(get_calls, any_order=True)
 
 
 def linked_quantity_reference(mock_access):
