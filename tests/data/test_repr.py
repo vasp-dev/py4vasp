2c026af2c25f1a31609c852942e98b82b7cf67c9
Martin Schlipf <martin.schlipf@gmail.com>
2022-06-14 10:50:24 +0200

    Update repr for new interface

--- tests/data/test_repr.py
+++ tests/data/test_repr.py
@@ -1,36 +1,30 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
-from py4vasp.raw import *
 from py4vasp.data import *
-from py4vasp._util.convert import to_snakecase
-from numpy import array
-import pytest
 
 
-@pytest.mark.xfail  # TODO: Adjust this for new interface
-def test_repr(raw_data):
-    tests = {
-        Band: "multiple",
-        Density: "Fe3O4 collinear",
-        DielectricFunction: "electron",
-        DielectricTensor: "dft",
-        Dos: "Fe3O4",
-        ElasticModulus: None,
-        Energy: None,
-        ForceConstant: "Sr2TiO4",
-        Force: "Sr2TiO4",
-        InternalStrain: "Sr2TiO4",
-        Kpoint: "line",
-        Magnetism: "collinear",
-        PiezoelectricTensor: None,
-        Polarization: None,
-        Projector: "Fe3O4",
-        Stress: "Sr2TiO4",
-        Structure: "Fe3O4",
-        Topology: "Fe3O4",
-    }
-    for class_, parameter in tests.items():
-        raw = getattr(raw_data, to_snakecase(class_.__name__))(parameter)
-        instance = class_(raw)
+def test_repr():
+    tests = [
+        Band,
+        Density,
+        DielectricFunction,
+        DielectricTensor,
+        Dos,
+        ElasticModulus,
+        Energy,
+        ForceConstant,
+        Force,
+        InternalStrain,
+        Kpoint,
+        Magnetism,
+        PiezoelectricTensor,
+        Polarization,
+        Projector,
+        Stress,
+        Structure,
+        Topology,
+    ]
+    for class_ in tests:
+        instance = class_.from_data("mock_data")
         copy = eval(repr(instance))
         assert copy.__class__ == class_
