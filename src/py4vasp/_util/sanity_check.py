759bbc8e593d6c45e3255a4344a04f320d5d87f1
Martin Schlipf <martin.schlipf@gmail.com>
2022-10-21 10:05:01 +0200

    Implement new sanity check for function arguments

--- src/py4vasp/_util/sanity_check.py
+++ src/py4vasp/_util/sanity_check.py
@@ -1,5 +1,6 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+import inspect
 import numbers
 import py4vasp.exceptions as exception
 
@@ -12,3 +13,12 @@ def raise_error_if_not_string(test_if_string, error_message):
 def raise_error_if_not_number(test_if_number, error_message):
     if not isinstance(test_if_number, numbers.Number):
         raise exception.IncorrectUsage(error_message)
+
+
+def raise_error_if_not_callable(function, *args, **kwargs):
+    signature = inspect.signature(function)
+    try:
+        signature.bind(*args, **kwargs)
+    except TypeError as error:
+        message = f"You tried to call {function.__name__}, but the arguments are incorrect! Please double check your input."
+        raise exception.IncorrectUsage(message) from error
--- tests/_util/test_sanity_check.py
+++ tests/_util/test_sanity_check.py
@@ -6,5 +6,30 @@ import pytest
 
 
 def test_error_if_not_string():
+    check.raise_error_if_not_string("string does not raise exception", "message")
     with pytest.raises(exception.IncorrectUsage):
         check.raise_error_if_not_string(1, "message")
+
+
+def test_error_if_not_number():
+    check.raise_error_if_not_number(1, "number does not raise exception")
+    with pytest.raises(exception.IncorrectUsage):
+        check.raise_error_if_not_number("should be number", "message")
+
+
+def test_error_if_not_callable():
+    def func(x, y=1):
+        pass
+
+    # valid calls do not raise error
+    check.raise_error_if_not_callable(func, 0)
+    check.raise_error_if_not_callable(func, 1, 2)
+    check.raise_error_if_not_callable(func, x=3, y=4)
+
+    # invalid calls raise error
+    with pytest.raises(exception.IncorrectUsage):
+        check.raise_error_if_not_callable(func, y=5)
+    with pytest.raises(exception.IncorrectUsage):
+        check.raise_error_if_not_callable(func, 6, 7, 8)
+    with pytest.raises(exception.IncorrectUsage):
+        check.raise_error_if_not_callable(func, 9, z=10)
