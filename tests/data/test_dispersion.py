cad3e0c186bba6536daf03bf49537c68e8f4a268
Martin Schlipf <martin.schlipf@gmail.com>
2022-10-21 16:04:20 +0200

    Generalize the test to run on all dispersion

--- tests/data/test_dispersion.py
+++ tests/data/test_dispersion.py
@@ -5,9 +5,9 @@ import types
 from py4vasp.data import Dispersion, Kpoint
 
 
-@pytest.fixture
-def single_band(raw_data):
-    raw_dispersion = raw_data.dispersion("single_band")
+@pytest.fixture(params=["single_band", "spin_polarized", "line", "phonon"])
+def dispersion(raw_data, request):
+    raw_dispersion = raw_data.dispersion(request.param)
     dispersion = Dispersion.from_data(raw_dispersion)
     dispersion.ref = types.SimpleNamespace()
     dispersion.ref.kpoints = Kpoint.from_data(raw_dispersion.kpoints)
@@ -15,8 +15,8 @@ def single_band(raw_data):
     return dispersion
 
 
-def test_read_single_band(single_band, Assert):
-    dispersion = single_band.read()
-    Assert.allclose(dispersion["kpoint_distances"], single_band.ref.kpoints.distances())
-    assert dispersion["kpoint_labels"] == single_band.ref.kpoints.labels()
-    Assert.allclose(dispersion["eigenvalues"], single_band.ref.eigenvalues)
+def test_read_dispersion(dispersion, Assert):
+    actual = dispersion.read()
+    Assert.allclose(actual["kpoint_distances"], dispersion.ref.kpoints.distances())
+    assert actual["kpoint_labels"] == dispersion.ref.kpoints.labels()
+    Assert.allclose(actual["eigenvalues"], dispersion.ref.eigenvalues)
