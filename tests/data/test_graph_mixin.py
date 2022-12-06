81a929977e6e47635cfeec3cee1997099a0f0f91
Martin Schlipf <martin.schlipf@gmail.com>
2022-12-06 10:11:09 +0100

    Add documentation and license statement

--- tests/data/test_graph_mixin.py
+++ tests/data/test_graph_mixin.py
@@ -1,3 +1,5 @@
+# Copyright © VASP Software GmbH,
+# Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 from pathlib import Path
 from unittest.mock import MagicMock
 
