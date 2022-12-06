6c850695b2d061953c732417ac03308c9c300603
Martin Schlipf <martin.schlipf@gmail.com>
2022-12-06 09:48:23 +0100

    Pass arguments to to_graph

--- src/py4vasp/_data/graph.py
+++ src/py4vasp/_data/graph.py
@@ -5,17 +5,17 @@ from py4vasp._util import convert
 
 class Mixin(abc.ABC):
     @abc.abstractmethod
-    def to_graph(self):
+    def to_graph(self, *args, **kwargs):
         pass
 
-    def plot(self):
-        return self.to_graph()
+    def plot(self, *args, **kwargs):
+        return self.to_graph(*args, **kwargs)
 
-    def to_plotly(self):
-        return self.to_graph().to_plotly()
+    def to_plotly(self, *args, **kwargs):
+        return self.to_graph(*args, **kwargs).to_plotly()
 
-    def to_image(self, *, filename=None):
-        fig = self.to_plotly()
+    def to_image(self, *args, filename=None, **kwargs):
+        fig = self.to_plotly(*args, **kwargs)
         classname = convert.to_snakecase(self.__class__.__name__).strip("_")
         filename = filename if filename is not None else f"{classname}.png"
         fig.write_image(self._path / filename)
--- tests/data/test_graph_mixin.py
+++ tests/data/test_graph_mixin.py
@@ -51,3 +51,43 @@ def test_filename_is_keyword_only_argument():
     example = ExampleGraph()
     with pytest.raises(TypeError):
         example.to_image("example.jpg")
+
+
+class ExampleWithArguments(graph.Mixin):
+    _path = Path("/absolute/path")
+
+    def to_graph(self, mandatory, optional=None):
+        GRAPH.reset_mock()
+        GRAPH.arguments = {"mandatory": mandatory, "optional": optional}
+        GRAPH.optional = optional
+        return GRAPH
+
+
+def test_arguments_passed_by_plot():
+    example = ExampleWithArguments()
+    graph = example.plot("only mandatory")
+    assert graph.arguments == {"mandatory": "only mandatory", "optional": None}
+    graph = example.plot("first", "second")
+    assert graph.arguments == {"mandatory": "first", "optional": "second"}
+    graph = example.plot(optional="foo", mandatory="bar")
+    assert graph.arguments == {"mandatory": "bar", "optional": "foo"}
+
+
+def test_arguments_passed_by_to_plotly():
+    example = ExampleWithArguments()
+    example.to_plotly("only mandatory")
+    assert GRAPH.arguments == {"mandatory": "only mandatory", "optional": None}
+    example.to_plotly("first", "second")
+    assert GRAPH.arguments == {"mandatory": "first", "optional": "second"}
+    example.to_plotly(optional="foo", mandatory="bar")
+    assert GRAPH.arguments == {"mandatory": "bar", "optional": "foo"}
+
+
+def test_arguments_passed_by_to_image():
+    example = ExampleWithArguments()
+    example.to_image("only mandatory")
+    assert GRAPH.arguments == {"mandatory": "only mandatory", "optional": None}
+    example.to_image("first", "second", filename="example.png")
+    assert GRAPH.arguments == {"mandatory": "first", "optional": "second"}
+    example.to_image(optional="foo", mandatory="bar")
+    assert GRAPH.arguments == {"mandatory": "bar", "optional": "foo"}
