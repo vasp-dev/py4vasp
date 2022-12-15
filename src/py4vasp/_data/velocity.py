87dbba51baa1fba5ef1606a549fab0026ec5f77c
Martin Schlipf <martin.schlipf@gmail.com>
2022-12-15 15:47:52 +0100

    Document the Velocity class

--- src/py4vasp/_data/velocity.py
+++ src/py4vasp/_data/velocity.py
@@ -5,11 +5,19 @@ import numpy as np
 from py4vasp import exception
 from py4vasp._config import VASP_GRAY
 from py4vasp._data import base, slice_, structure
-from py4vasp._util import convert, reader
+from py4vasp._util import convert, reader, documentation
 
 
+@documentation.format(examples=slice_.examples("velocity"))
 class Velocity(slice_.Mixin, base.Refinery, structure.Mixin):
-    "The ion velocities for all steps of the calculation."
+    """The ion velocities for all steps of the calculation.
+
+    The velocities are only stored if you set VELOCITY = T in the INCAR file. You can
+    read velocities of different steps see the examples below.
+
+    {examples}
+    """
+
     velocity_rescale = 1
 
     @base.data_access
@@ -28,14 +36,38 @@ class Velocity(slice_.Mixin, base.Refinery, structure.Mixin):
         return f"{element:21.16f}"
 
     @base.data_access
+    @documentation.format(examples=slice_.examples("velocity", "to_dict"))
     def to_dict(self):
+        """Return the structure and ion velocities in a dictionary
+
+        Returns
+        -------
+        dict
+            The dictionary contains the ion velocities as well as the structural
+            information for reference.
+
+        {examples}
+        """
         return {
             "structure": self._structure[self._steps].read(),
             "velocities": self._velocity[self._steps],
         }
 
     @base.data_access
+    @documentation.format(examples=slice_.examples("velocity", "plot"))
     def plot(self):
+        """Plot the velocities as vectors in the structure.
+
+        This is currently only implemented for a single step. So selecting multiple
+        steps will raise an error.
+
+        Returns
+        -------
+        Viewer3d
+            Contains all atoms and the velocities are drawn as vectors.
+
+        {examples}
+        """
         self._raise_error_if_slice()
         velocities = self.velocity_rescale * self._velocity[self._steps]
         viewer = self._structure.plot()
