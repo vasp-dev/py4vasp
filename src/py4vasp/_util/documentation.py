7431dbd4293bd172edc774c4c01fe1429d53f24d
Martin Schlipf <martin.schlipf@gmail.com>
2022-12-16 17:17:00 +0100

    Remove old documentation.add

--- src/py4vasp/_util/documentation.py
+++ src/py4vasp/_util/documentation.py
@@ -3,14 +3,6 @@
 import inspect
 
 
-def add(doc_string):
-    def add_documentation_to_function(func):
-        func.__doc__ = doc_string
-        return func
-
-    return add_documentation_to_function
-
-
 def format(**kwargs):
     def format_documentation_of_function(func):
         clean_kwargs = {key: str(value).rstrip() for key, value in kwargs.items()}
