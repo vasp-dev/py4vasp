2648479ff1f5226de114e087285d0337e1abdef9
Martin Schlipf <martin.schlipf@gmail.com>
2023-01-18 16:23:48 +0100

    Prepare version number for release

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, VASP Software GmbH"
 author = "VASP Software GmbH"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.7.0"
 
 
 # -- General configuration ---------------------------------------------------
