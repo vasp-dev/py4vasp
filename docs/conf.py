a72a5bb66391a081c5b927ed9e6beb885792940c
Martin Schlipf <martin.schlipf@gmail.com>
2023-02-17 16:51:58 +0100

    Release version 0.7.1 (#79)

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, VASP Software GmbH"
 author = "VASP Software GmbH"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.0"
+release = "0.7.1"
 
 
 # -- General configuration ---------------------------------------------------
