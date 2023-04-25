9d07266fc5795862757e6f09c7c18c6745449595
Martin Schlipf <martin.schlipf@gmail.com>
2023-04-25 09:47:59 +0200

    Implement selecting multiple components

--- src/py4vasp/_util/index.py
+++ src/py4vasp/_util/index.py
@@ -1,30 +1,32 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
-from __future__ import annotations
-
-import dataclasses
-
 import numpy as np
+
+from py4vasp import exception
 
 
 class Selector:
     def __init__(self, maps, data):
         self._data = data
+        self._axes = tuple(maps.keys())
         self._map = {
-            key: (dim, _make_1d(indices))
-            for dim, map in maps.items()
-            for key, indices in map.items()
+            key: (dim, _make_slice(indices))
+            for dim, map_ in maps.items()
+            for key, indices in map_.items()
         }
 
-    def __getitem__(self, key):
-        selection = [slice(None)] * self._data.ndim
-        dim, indices = self._map[key]
-        selection[dim] = indices
-        return np.sum(self._data[tuple(selection)], axis=-1)
+    def __getitem__(self, selection):
+        indices = [slice(None)] * self._data.ndim
+        for key in selection:
+            dimension, slice_ = self._map[key]
+            indices[dimension] = slice_
+        return np.sum(self._data[tuple(indices)], axis=self._axes)
 
 
-def _make_1d(indices):
+def _make_slice(indices):
     if isinstance(indices, int):
-        return [indices]
-    else:
+        return slice(indices, indices + 1)
+    if isinstance(indices, slice):
         return indices
+    message = f"A conversion of {indices} to slice is not implemented."
+    raise exception.NotImplemented(message)
--- tests/util/test_index.py
+++ tests/util/test_index.py
@@ -3,28 +3,50 @@
 import numpy as np
 import pytest
 
+from py4vasp import exception
 from py4vasp._util import index
 
 
-@pytest.mark.parametrize("input, output", [("Sr", 1), ("Ti", 2), ("O", 3)])
-def test_one_component(input, output):
+@pytest.mark.parametrize("selection, indices", [("Sr", 1), ("Ti", 2), ("O", 3)])
+def test_one_component(selection, indices):
     values = np.arange(10) ** 2
-    map = {0: {"Sr": 1, "Ti": 2, "O": 3}}
-    selector = index.Selector(map, values)
-    assert selector[input] == values[output]
+    map_ = {0: {"Sr": 1, "Ti": 2, "O": 3}}
+    selector = index.Selector(map_, values)
+    assert selector[(selection,)] == values[indices]
 
 
-@pytest.mark.parametrize("input, output", [("A", [1, 2]), ("B", [3, 5, 7])])
-def test_sum_over_selection(input, output):
+@pytest.mark.parametrize("selection, indices", [("A", [1, 2]), ("B", [3, 5, 7])])
+def test_sum_over_selection(selection, indices):
     values = np.arange(10) ** 2
-    map = {0: {"A": [1, 2], "B": slice(3, 8, 2)}}
-    selector = index.Selector(map, values)
-    assert selector[input] == np.sum(values[output])
+    map_ = {0: {"A": slice(1, 3), "B": slice(3, 8, 2)}}
+    selector = index.Selector(map_, values)
+    assert selector[(selection,)] == np.sum(values[indices])
 
 
-@pytest.mark.parametrize("input, output", [("x", [7]), ("y", [2, 5])])
-def test_two_components(input, output):
+@pytest.mark.parametrize("selection, indices", [("x", [7]), ("y", [2, 5])])
+def test_select_one_of_two_components(selection, indices):
     values = np.arange(30).reshape((3, 10))
-    map = {1: {"x": 7, "y": [2, 5]}}
-    selector = index.Selector(map, values)
-    assert np.all(selector[input] == np.sum(values[:, output], axis=1))
+    map_ = {1: {"x": 7, "y": slice(2, 6, 3)}}
+    selector = index.Selector(map_, values)
+    assert np.all(selector[(selection,)] == np.sum(values[:, indices], axis=1))
+
+
+@pytest.mark.parametrize(
+    "selection, indices",
+    [
+        (("A",), (1, slice(None))),
+        (("x",), (slice(None), 1)),
+        (("B", "z"), (2, 3)),
+        (("y", "C"), (3, 2)),
+    ],
+)
+def test_select_two_of_two_components(selection, indices):
+    values = np.arange(30).reshape((6, 5))
+    map_ = {0: {"A": 1, "B": 2, "C": 3}, 1: {"x": 1, "y": 2, "z": 3}}
+    selector = index.Selector(map_, values)
+    assert selector[selection] == np.sum(values[indices])
+
+
+def test_error_when_indices_are_not_int_or_slice():
+    with pytest.raises(exception.NotImplemented):
+        index.Selector({0: {"A": [1, 2]}}, None)
