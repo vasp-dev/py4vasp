690653458b19cd7f29186f382a938691bcc1ef51
Marie-Therese Huebsch <marie-therese.huebsch@vasp.at>
2023-12-17 16:42:44 +0100

    consider 3 components of magnetization in reference output (test fix)

--- tests/data/test_density.py
+++ tests/data/test_density.py
@@ -115,8 +115,8 @@ def check_plotting_magnetization_density(polarized_density, Assert):
         reference_magnetization = polarized_density.ref.output["magnetization"].T
         check_magnetization_plot(reference_magnetization, calls, Assert)
     elif polarized_density.is_noncollinear():
-        with cm_init as init, cm_cell as cell, cm_surface as surface:
-            for component in [1,2,3]:
+        for component in [1,2,3]:
+            with cm_init as init, cm_cell as cell, cm_surface as surface:
                 result = polarized_density.plot("magnetization("+str(component)+")", isolevel=0.1, smooth=1)
                 assert isinstance(result, viewer3d.Viewer3d)
                 calls = surface.call_args_list
