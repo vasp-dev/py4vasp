eae29fb89296c3c89bade41621a9b040a62af103
Martin Schlipf <martin.schlipf@gmail.com>
2024-01-25 14:15:42 +0100

    Move Potential to calculation module

--- src/py4vasp/data.py
+++ src/py4vasp/data.py
@@ -41,7 +41,7 @@ from py4vasp.calculation._phonon_band import PhononBand
 from py4vasp.calculation._phonon_dos import PhononDos
 from py4vasp.calculation._piezoelectric_tensor import PiezoelectricTensor
 from py4vasp.calculation._polarization import Polarization
-from py4vasp._data.potential import Potential
+from py4vasp.calculation._potential import Potential
 from py4vasp._data.projector import Projector
 from py4vasp._data.stress import Stress
 from py4vasp._data.structure import Structure
