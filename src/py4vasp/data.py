0cc3e6d842d4b0bde77e786da0f2b3dd44bc3d11
Martin Schlipf <martin.schlipf@gmail.com>
2024-01-25 13:55:16 +0100

    Move ElasticModulus to calculation module

--- src/py4vasp/data.py
+++ src/py4vasp/data.py
@@ -28,7 +28,7 @@ from py4vasp.calculation._dielectric_function import DielectricFunction
 from py4vasp.calculation._dielectric_tensor import DielectricTensor
 from py4vasp.calculation._dispersion import Dispersion
 from py4vasp.calculation._dos import Dos
-from py4vasp._data.elastic_modulus import ElasticModulus
+from py4vasp.calculation._elastic_modulus import ElasticModulus
 from py4vasp._data.energy import Energy
 from py4vasp._data.fatband import Fatband
 from py4vasp._data.force import Force
