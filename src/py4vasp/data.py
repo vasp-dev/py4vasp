cacb5d1aa9b4b2c8d0495f54e10a1e8d8aed9872
Sudarshan Vijay <sudarshan.vijay@vasp.at>
2024-02-02 10:44:59 +0100

    Outline of oszicar to_dict

--- src/py4vasp/data.py
+++ src/py4vasp/data.py
@@ -36,6 +36,7 @@ from py4vasp._data.force_constant import ForceConstant
 from py4vasp._data.internal_strain import InternalStrain
 from py4vasp._data.kpoint import Kpoint
 from py4vasp._data.magnetism import Magnetism
+from py4vasp._data.oszicar import OSZICAR
 from py4vasp._data.pair_correlation import PairCorrelation
 from py4vasp._data.phonon_band import PhononBand
 from py4vasp._data.phonon_dos import PhononDos
