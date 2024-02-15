3c12ad51468af3f157ae2fe430444f9cf5c107c4
Martin Schlipf <martin.schlipf@gmail.com>
2024-02-15 09:44:25 +0100

    Add attributes to View for Isosurfaces

--- src/py4vasp/_third_party/view/__init__.py
+++ src/py4vasp/_third_party/view/__init__.py
@@ -1,4 +1,4 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 from .mixin import Mixin
-from .view import View
+from .view import GridQuantity, View
