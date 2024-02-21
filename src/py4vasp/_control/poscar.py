34398f836a37a398e37c3d0eb07a429e1ff782d0
Martin Schlipf <martin.schlipf@gmail.com>
2024-02-21 08:35:10 +0100

    Adjust POSCAR to new viewer

--- src/py4vasp/_control/poscar.py
+++ src/py4vasp/_control/poscar.py
@@ -2,9 +2,10 @@
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 from py4vasp import calculation
 from py4vasp._control import base
+from py4vasp._third_party import view
 
 
-class POSCAR(base.InputFile):
+class POSCAR(base.InputFile, view.Mixin):
     """The POSCAR file defining the structure used in the VASP calculation.
 
     Parameters
@@ -14,7 +15,7 @@ class POSCAR(base.InputFile):
         in memory.
     """
 
-    def plot(self, *args, **kwargs):
+    def to_view(self, supercell=None):
         """Generate a 3d representation of the structure in the file.
 
         Parameters
@@ -25,7 +26,7 @@ class POSCAR(base.InputFile):
 
         Returns
         -------
-        Viewer3d
+        View
             Visualize the structure as a 3d figure.
         """
-        return calculation.structure.from_POSCAR(self).plot(*args, **kwargs)
+        return calculation.structure.from_POSCAR(self).plot(supercell)
