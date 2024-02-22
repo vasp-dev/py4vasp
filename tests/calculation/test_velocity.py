2ce46d9de58aa6e2859258599fe073feb1596c3d
Martin Schlipf <martin.schlipf@gmail.com>
2024-02-22 09:18:31 +0100

    Adjust Velocity for new View class

--- tests/calculation/test_velocity.py
+++ tests/calculation/test_velocity.py
@@ -1,18 +1,29 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 import types
-from unittest.mock import patch
 
 import pytest
 
-from py4vasp import calculation, exception
-from py4vasp._config import VASP_GRAY
-from py4vasp._util import convert
+from py4vasp import _config, calculation, exception
+
+
+@pytest.fixture
+def Sr2TiO4(raw_data):
+    return create_velocity_data(raw_data, "Sr2TiO4")
 
 
 @pytest.fixture(params=["Sr2TiO4", "Fe3O4"])
 def velocities(raw_data, request):
-    raw_velocity = raw_data.velocity(request.param)
+    return create_velocity_data(raw_data, request.param)
+
+
+@pytest.fixture(params=[-1, 0, slice(None), slice(1, 3)])
+def steps(request):
+    return request.param
+
+
+def create_velocity_data(raw_data, structure):
+    raw_velocity = raw_data.velocity(structure)
     velocity = calculation.velocity.from_data(raw_velocity)
     velocity.ref = types.SimpleNamespace()
     velocity.ref.structure = calculation.structure.from_data(raw_velocity.structure)
@@ -20,63 +31,27 @@ def velocities(raw_data, request):
     return velocity
 
 
-@pytest.fixture
-def Fe3O4(raw_data):
-    raw_velocity = raw_data.velocity("Fe3O4")
-    velocity = calculation.velocity.from_data(raw_velocity)
-    velocity.ref = types.SimpleNamespace()
-    velocity.ref.structure = calculation.structure.from_data(raw_velocity.structure)
-    velocity.ref.velocities = raw_velocity.velocities
-    return velocity
+def test_read(velocities, steps, Assert):
+    actual = velocities.read() if steps == -1 else velocities[steps].read()
+    reference_structure = velocities.ref.structure[steps].read()
+    Assert.same_structure(actual["structure"], reference_structure)
+    Assert.allclose(actual["velocities"], velocities.ref.velocities[steps])
 
 
-def test_read_Sr2TiO4(Sr2TiO4, Assert):
-    check_read_velocity(Sr2TiO4.read(), Sr2TiO4.ref, -1, Assert)
-    for steps in (slice(None), slice(1, 3), 0):
-        check_read_velocity(Sr2TiO4[steps].read(), Sr2TiO4.ref, steps, Assert)
-
-
-def test_read_Fe3O4(Fe3O4, Assert):
-    check_read_velocity(Fe3O4.read(), Fe3O4.ref, -1, Assert)
-    for steps in (slice(None), slice(1, 3), 0):
-        check_read_velocity(Fe3O4[steps].read(), Fe3O4.ref, steps, Assert)
-
-
-def check_read_velocity(actual, reference, steps, Assert):
-    reference_structure = reference.structure[steps].read()
-    for key in actual["structure"]:
-        if key in ("elements", "names"):
-            assert actual["structure"][key] == reference_structure[key]
-        else:
-            Assert.allclose(actual["structure"][key], reference_structure[key])
-    Assert.allclose(actual["velocities"], reference.velocities[steps])
-
-
-def test_plot_Sr2TiO4(Sr2TiO4, Assert):
-    check_plot_velocity(Sr2TiO4, -1, Assert)
-    check_plot_velocity(Sr2TiO4, 0, Assert)
-    for steps in (slice(None), slice(1, 3)):
-        with pytest.raises(exception.NotImplemented):
-            Sr2TiO4[steps].plot()
-
-
-def test_plot_Fe3O4(Fe3O4, Assert):
-    check_plot_velocity(Fe3O4, -1, Assert)
-    check_plot_velocity(Fe3O4, 0, Assert)
-
-
-def check_plot_velocity(velocity, step, Assert):
-    with patch("py4vasp.calculation._structure.Structure.plot") as plot:
-        if step == -1:
-            velocity.plot()
-        else:
-            velocity[step].plot()
-        plot.assert_called_once()
-        viewer = plot.return_value
-        viewer.show_arrows_at_atoms.assert_called_once()
-        args, _ = viewer.show_arrows_at_atoms.call_args
-    Assert.allclose(args[0], velocity.velocity_rescale * velocity.ref.velocities[step])
-    Assert.allclose(args[1], convert.to_rgb(VASP_GRAY))
+@pytest.mark.parametrize("supercell", [None, 2, (3, 2, 1)])
+def test_plot(velocities, steps, supercell, Assert):
+    structure_view = velocities.ref.structure.plot(supercell)
+    plot_method = velocities.plot if steps == -1 else velocities[steps].plot
+    view = plot_method(supercell) if supercell else plot_method()
+    Assert.same_structure_view(view, structure_view)
+    assert len(view.ion_arrows) == 1
+    arrows = view.ion_arrows[0]
+    assert arrows.quantity.ndim == 3
+    expected_velocities = velocities.velocity_rescale * velocities.ref.velocities[steps]
+    Assert.allclose(arrows.quantity, expected_velocities)
+    assert arrows.label == "velocities"
+    assert arrows.color == _config.VASP_GRAY
+    assert arrows.radius == 0.2
 
 
 def test_incorrect_access(Sr2TiO4):
@@ -129,7 +104,6 @@ def test_print_Sr2TiO4(Sr2TiO4, format_):
     assert actual == {"text/plain": ref_plain}
 
 
-@pytest.mark.xfail
 def test_factory_methods(raw_data, check_factory_methods):
     data = raw_data.velocity("Fe3O4")
     check_factory_methods(calculation.velocity, data)
