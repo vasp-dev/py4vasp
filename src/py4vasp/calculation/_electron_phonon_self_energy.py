fb734676592633edd4020405fbc5e856cb80cb1f
Martin Schlipf <martin.schlipf@gmail.com>
2024-10-25 10:08:15 +0200

    Add minimal example of a read function

--- src/py4vasp/calculation/_electron_phonon_self_energy.py
+++ src/py4vasp/calculation/_electron_phonon_self_energy.py
@@ -1,6 +1,18 @@
-from py4vasp.calculation import _base
+# Copyright © VASP Software GmbH,
+# Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+from py4vasp.calculation import _base, _slice
 
 
-class ElectronPhononSelfEnergy(_base.Refinery):
+class ElectronPhononSelfEnergy(_slice.Mixin, _base.Refinery):
     "Placeholder for electron phonon self energy"
-    pass
+
+    def to_dict(self):
+        return {
+            "eigenvalues": self._raw_data.eigenvalues[:],
+            "debye_waller": self._read_slice_of_data("debye_waller"),
+            "fan": self._read_slice_of_data("fan"),
+        }
+
+    def _read_slice_of_data(self, name):
+        slice_of_data = getattr(self._raw_data, name)[self._steps]
+        return [data[:] for data in slice_of_data]
--- tests/calculation/electron_phonon/test_self_energy.py
+++ tests/calculation/electron_phonon/test_self_energy.py
@@ -11,8 +11,25 @@ from py4vasp import calculation
 def self_energy(raw_data):
     raw_self_energy = raw_data.electron_phonon.self_energy("default")
     self_energy = calculation.electron_phonon.self_energy.from_data(raw_self_energy)
+    self_energy.ref = types.SimpleNamespace()
+    self_energy.ref.eigenvalues = raw_self_energy.eigenvalues
+    self_energy.ref.debye_waller = raw_self_energy.debye_waller
+    self_energy.ref.fan = raw_self_energy.fan
     return self_energy
 
 
-def test_read(self_energy):
-    assert True
+@pytest.fixture(params=[None, 0, slice(1, 3), slice(None)])
+def sample(request):
+    return request.param
+
+
+def test_read(self_energy, sample, Assert):
+    if sample is None:
+        slice_ = -1
+        actual = self_energy.read()
+    else:
+        slice_ = sample
+        actual = self_energy[sample].read()
+    Assert.allclose(actual["eigenvalues"], self_energy.ref.eigenvalues)
+    Assert.allclose(actual["debye_waller"], self_energy.ref.debye_waller[slice_])
+    Assert.allclose(actual["fan"], self_energy.ref.fan[slice_])
