57b17516ab5d6b346eb3f435507c35edcc04a003
Sudarshan Vijay <sudarshan.vijay@vasp.at>
2024-11-12 11:09:13 +0100

    rename oszicar to electronic minimzation

--- src/py4vasp/_raw/definition.py
+++ src/py4vasp/_raw/definition.py
@@ -379,7 +379,7 @@ schema.add(
 )
 #
 schema.add(
-    raw.OSZICAR,
+    raw.ElectronicMinimization,
     required=raw.Version(6, 5),
     label="intermediate/ion_dynamics/oszicar_label",
     convergence_data="intermediate/ion_dynamics/oszicar",
