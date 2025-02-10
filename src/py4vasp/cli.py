a3f7e2f416d639e2f78a25fa4b4c3016cfbf310f
Martin Schlipf <martin.schlipf@gmail.com>
2025-02-10 17:03:46 +0100

    Implement default conversion to LAMMPS

--- src/py4vasp/cli.py
+++ src/py4vasp/cli.py
@@ -1,6 +1,10 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+import pathlib
+
 import click
+
+import py4vasp
 
 
 @click.group()
@@ -9,12 +13,26 @@ def cli():
 
 
 @cli.command()
-@click.argument("source", type=click.Choice(("structure",)), metavar="SOURCE")
-@click.argument("destination", type=click.STRING)
-def convert(source):
+@click.argument("quantity", type=click.Choice(("structure",)), metavar="QUANTITY")
+@click.argument("format", type=click.STRING)
+@click.option(
+    "-f",
+    "--from",
+    "path",
+    type=click.Path(exists=True, readable=True),
+    help="Overwrite the default path where py4vasp looks for the quantity.",
+)
+@click.option(
+    "-s",
+    "--selection",
+    type=click.STRING,
+    help="String to further clarify the specific source of the quantity.",
+)
+def convert(quantity, format, path, selection):
     """Convert a quantity to a different format.
 
-    SOURCE is the name of the quantity that you want to convert.
-    DESTINATION is the target format to which you want to convert.
+    Specify which QUANTITY you want to convert into which FORMAT.
     """
-    pass
+    path = path or pathlib.Path.cwd()
+    calculation = py4vasp.Calculation.from_path(path)
+    print(calculation.structure.to_lammps())
--- tests/cli/test_convert.py
+++ tests/cli/test_convert.py
@@ -1,13 +1,30 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+import pathlib
+from unittest.mock import patch
+
 import pytest
 from click.testing import CliRunner
 
 from py4vasp.cli import cli
 
 
+@pytest.fixture
+def mock_calculation():
+    with patch("py4vasp.Calculation", autospec=True) as mock:
+        yield mock
+
+
 @pytest.mark.parametrize("lammps", ("LAMMPS", "Lammps", "lammps"))
-def test_convert_lammps(lammps):
+def test_convert_lammps(mock_calculation, lammps):
     runner = CliRunner()
-    result = runner.invoke(cli, ["convert", lammps])
+    result = runner.invoke(cli, ["convert", "structure", lammps])
+    print(result.output)
     assert result.exit_code == 0
+    expected_path = pathlib.Path.cwd()
+    constructor = mock_calculation.from_path
+    constructor.assert_called_once_with(expected_path)
+    structure = constructor.return_value.structure
+    structure.to_lammps.assert_called_once_with()
+    converted = structure.to_lammps.return_value
+    assert f"{converted}\n" == result.output
