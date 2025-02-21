5d5f93257bc983f3895c1f8deeb6de46ddb2fbd4
Martin Schlipf <martin.schlipf@gmail.com>
2025-02-21 11:00:28 +0100

    Fix formatting

--- tests/util/test_suggest.py
+++ tests/util/test_suggest.py
@@ -14,5 +14,6 @@ def test_no_similar_selection():
 def test_key_is_converted_to_string():
     assert suggest.did_you_mean(120, ["99", "121", "700"]) == 'Did you mean "121"? '
 
+
 def test_possibilities_are_converted_to_string():
     assert suggest.did_you_mean("320", range(291, 330, 10)) == 'Did you mean "321"? '
