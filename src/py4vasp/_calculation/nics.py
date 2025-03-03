0bc4a96cb232a2049d9ddba22c487dbef278e72d
Martin Schlipf <martin.schlipf@gmail.com>
2025-03-03 12:01:09 +0100

    Implemented NICS and tests for reading.

--- src/py4vasp/_calculation/nics.py
+++ src/py4vasp/_calculation/nics.py
@@ -1,3 +1,9 @@
+# Copyright © VASP Software GmbH,
+# Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+
+
+import numpy as np
+
 from py4vasp._calculation import base, structure
 
 
@@ -16,6 +22,22 @@ class Nics(base.Refinery, structure.Mixin):
         dict
             Contains the structure information as well as the nucleus-independent chemical shift represented on a grid in the unit cell.
         """
-        result = {"structure": self._structure.read()}
-        # result.update(self._read_nics())
+        result = {
+            "structure": self._structure.read(),
+            "nics": self.to_numpy(),
+        }
         return result
+
+    @base.data_access
+    def to_numpy(self):
+        """Convert NICS to a numpy array.
+
+        Returns
+        -------
+        np.ndarray
+            All components of NICS.
+        """
+        transposed_nics = np.array(self._raw_data.nics).T
+        curr_shape = transposed_nics.shape
+        transposed_nics = transposed_nics.reshape((*curr_shape[:-1],3,3))
+        return transposed_nics
--- tests/calculation/test_nics.py
+++ tests/calculation/test_nics.py
@@ -8,14 +8,25 @@ import numpy as np
 import pytest
 
 from py4vasp._calculation.nics import Nics
+from py4vasp._calculation.structure import Structure
 
 
 @pytest.fixture
 def chemical_shift(raw_data):
     raw_nics = raw_data.nics("Sr2TiO4")
     nics = Nics.from_data(raw_nics)
+    nics.ref = types.SimpleNamespace()
+    transposed_nics = np.array(raw_nics.nics).T
+    nics.ref.structure = Structure.from_data(raw_nics.structure)
+    nics.ref.output = {
+        "nics": transposed_nics.reshape((10,12,14,3,3)),
+    }
     return nics
 
 
 def test_read(chemical_shift, Assert):
     actual = chemical_shift.read()
+    actual_structure = actual.pop("structure")
+    Assert.same_structure(actual_structure, chemical_shift.ref.structure.read())
+    assert actual.keys() == chemical_shift.ref.output.keys()
+    Assert.allclose(actual["nics"], chemical_shift.ref.output["nics"])
