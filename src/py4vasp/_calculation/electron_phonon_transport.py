b52afa9f4121c69b4ee3b524efaafcf0ee7f00f4
Henrique Miranda <miranda.henrique@vasp.at>
2025-04-25 16:17:43 +0200

    Fix imports in electron_phonon_transport.py

--- src/py4vasp/_calculation/electron_phonon_transport.py
+++ src/py4vasp/_calculation/electron_phonon_transport.py
@@ -1,16 +1,16 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
-from py4vasp.calculation import _base, _slice
+from py4vasp._calculation import base, slice_
 
 
-class ElectronPhononTransport(_slice.Mixin, _base.Refinery):
+class ElectronPhononTransport(slice_.Mixin, base.Refinery):
     "Placeholder for electron phonon transport"
 
-    @_base.data_access
+    @base.data_access
     def __str__(self):
         return "electron phonon transport"
 
-    @_base.data_access
+    @base.data_access
     def to_dict(self):
         return {
             "transport_function": self._read_slice_of_data("transport_function"),
