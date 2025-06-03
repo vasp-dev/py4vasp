903ff0fde4a5e017ee106bb987d2112b5d4e6028
Henrique Miranda <miranda.henrique@gmail.com>
2025-06-03 16:48:33 +0200

    Fix valid indices check in _get_valid_indices method to handle None case

--- src/py4vasp/_raw/access.py
+++ src/py4vasp/_raw/access.py
@@ -137,7 +137,8 @@ class _State:
             return None
         valid_indices = self._get_dataset(h5f, data.valid_indices)
         if hasattr(valid_indices, "is_none"):
-            return range(0)
+            if valid_indices.is_none():
+                return range(0)
         if valid_indices.ndim == 0:
             return range(valid_indices)
         else:
