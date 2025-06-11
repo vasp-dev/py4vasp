7ec4ad4964aa6d91a3a5607ce8224d2bbf9829d5
Henrique Miranda <miranda.henrique@gmail.com>
2025-06-11 11:27:59 +0200

    Just formatting

--- src/py4vasp/_raw/data.py
+++ src/py4vasp/_raw/data.py
@@ -292,6 +292,7 @@ class ElectronPhononBandgap(mapping.Mapping):
     scattering_approximation: str
     "Scattering approximation used to compute the electron self-energy"
 
+
 @dataclasses.dataclass
 class ElectronPhononSelfEnergy(mapping.Mapping):
     """The electron self-energy due to electron-phonon coupling
@@ -313,8 +314,8 @@ class ElectronPhononSelfEnergy(mapping.Mapping):
     "Chemical potential information"
     eigenvalues: VaspData
     "Kohn-sham eigenvalues on the mesh used for the electron-phonon calculation"
-    #temperatures: VaspData
-    #"List of temperatures at which the bandgap renormalization was computed"
+    # temperatures: VaspData
+    # "List of temperatures at which the bandgap renormalization was computed"
     debye_waller: VaspData
     "Debye-Waller self-energy contribution as an array flattened along (band,kpoint,spin)"
     fan: VaspData
