35a0d3c0033474955d9673d3667338bd15003dfe
Max Liebetreu <max.liebetreu@vasp.at>
2025-08-04 17:30:41 +0200

    Enhance Sphinx configuration by adding autodoc and napoleon extensions

--- tests/sphinx/examples/conf.py
+++ tests/sphinx/examples/conf.py
@@ -2,4 +2,13 @@
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 
 # dummy sphinx conf.py file
-extensions = ["py4vasp._sphinx"]
+extensions = [
+    "py4vasp._sphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon", 
+]
+
+# Configure autodoc to find the example module
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
