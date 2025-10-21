eb45249b0c7fa10b41e40b2172e1d39f088809c2
Martin Schlipf <martin.schlipf@gmail.com>
2025-10-21 12:35:03 +0200

    Update version number

--- src/py4vasp/__init__.py
+++ src/py4vasp/__init__.py
@@ -6,5 +6,5 @@ from py4vasp._third_party.graph import plot
 from py4vasp._third_party.interactive import set_error_handling
 from py4vasp.calculation._class import Calculation
 
-__version__ = "0.10.2"
+__version__ = "0.10.4"
 set_error_handling("Minimal")
