f926d09d3f0ac8110f685b6a3afd23dc261266b6
Max Liebetreu <max.liebetreu@vasp.at>
2026-01-29 18:34:04 +0100

    Change order of orbital_types

--- src/py4vasp/_calculation/projector.py
+++ src/py4vasp/_calculation/projector.py
@@ -134,7 +134,7 @@ class Projector(base.Refinery):
         return {
             "projector": {
                 "orbital_types": (
-                    list(self._init_orbital_dict().keys())
+                    sorted(list(self._init_orbital_dict().keys()), key=self._sort_key)
                     if not check.is_none(self._raw_data.orbital_types)
                     else None
                 )
