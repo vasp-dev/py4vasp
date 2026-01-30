Fix local moment calculation and tests; add tests for stoichiometry and expand database formula and compound calculation

src/py4vasp/_calculation/local_moment.py
@@ -159,27 +159,19 @@ class LocalMoment(slice_.Mixin, base.Refinery, structure.Mixin, view.Mixin):
 
     @base.data_access
     def _to_database(self, *args, **kwargs):
-        spin_moments_total = None
+        spin_moments_orbitals = None
         if not check.is_none(self._raw_data.spin_moments):
-            if self._is_nonpolarized or self._is_noncollinear:
-                spin_moments_total = self._raw_data.spin_moments[-1, 0]
-            elif self._is_collinear:
-                spin_moments_total = (
-                    self._raw_data.spin_moments[-1, 0]
-                    + self._raw_data.spin_moments[-1, 1]
-                )
+            if not self._is_nonpolarized:
+                spin_moments_orbitals = self._raw_data.spin_moments[-1, -1]
 
         spin_moment_total_min = None
         spin_moment_total_max = None
-        if spin_moments_total is not None:
-            # TODO make sure this is correctly understood
-            spin_moments_total_norm = (
-                np.linalg.norm(spin_moments_total, axis=-1)
-                if not self._is_noncollinear
-                else np.linalg.norm(spin_moments_total[:, 1:], axis=-1)
-            )
-            spin_moment_total_min = float(np.min(spin_moments_total_norm))
-            spin_moment_total_max = float(np.max(spin_moments_total_norm))
+        if spin_moments_orbitals is not None:
+            spin_moments_total = np.sum(
+                spin_moments_orbitals, axis=-1
+            )  # sum over orbitals
+            spin_moment_total_min = float(np.min(spin_moments_total))
+            spin_moment_total_max = float(np.max(spin_moments_total))
 
         data_dict = {
             "local_moment": {

tests/calculation/test_local_moment.py
@@ -314,22 +314,24 @@ def test_incorrect_step(example_moments):
         example_moments[out_of_bounds].charge()
 
 
-@pytest.mark.xfail
 def test_to_database(example_moments):
     db_dict = example_moments._read_to_database()["local_moment:default"]
     orbital_moments = getattr(example_moments.ref, "orbital_moments", None)
     assert db_dict["has_orbital_moments"] == (orbital_moments is not None)
-    # TODO correct
-    total_final_spin_moment = getattr(example_moments.ref, "magnetic", None)
+
+    total_final_spin_moment = getattr(example_moments.ref, "spin_moments", None)
+    sums_moments = None
+    if total_final_spin_moment is not None:
+        if example_moments._is_collinear:
+            total_final_spin_moment = total_final_spin_moment[-1, :]
+        elif example_moments._is_noncollinear:
+            total_final_spin_moment = total_final_spin_moment[-1, :, :, -1]
+        sums_moments = np.sum(total_final_spin_moment, axis=-1)
     assert db_dict["final_spin_moment_total_min"] == (
-        total_final_spin_moment[-1].shape
-        if (total_final_spin_moment is not None)
-        else None
+        np.min(sums_moments) if (sums_moments is not None) else None
     )
     assert db_dict["final_spin_moment_total_max"] == (
-        float(np.max(total_final_spin_moment[-1]))
-        if (total_final_spin_moment is not None)
-        else None
+        np.max(sums_moments) if (sums_moments is not None) else None
     )
 
 