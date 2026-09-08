0e3b893da5252d0ca17018e0f836db0cc3603231
Martin Schlipf <martin.schlipf@gmail.com>
2026-09-08 15:15:09 +0200

    Fix: restore print and selections on Force

--- tests/calculation/test_force.py
+++ tests/calculation/test_force.py
@@ -138,6 +138,15 @@ def test_to_database(forces):
         )
 
 
+def test_print_writes_to_stdout(Sr2TiO4, capsys):
+    assert Sr2TiO4.print() is None
+    assert capsys.readouterr().out == str(Sr2TiO4) + "\n"
+
+
+def test_selections(Sr2TiO4):
+    assert Sr2TiO4.selections() == {"force": ["default"]}
+
+
 def test_factory_methods(raw_data, check_factory_methods):
     data = raw_data.force("Fe3O4")
-    check_factory_methods(Force, data)
+    check_factory_methods(Force, data, skip_methods=["selections"])
