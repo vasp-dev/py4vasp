f9da7daa0d214dcc73389ee363f061d0317f034e
Martin Schlipf <martin.schlipf@gmail.com>
2026-09-08 15:16:43 +0200

    Fix: restore print on Optics

--- src/py4vasp/_calculation/optics.py
+++ src/py4vasp/_calculation/optics.py
@@ -560,6 +560,17 @@ class Optics(graph.Mixin):
             OpticsHandler.selections,
         )
 
+    def print(self, selection: str | None = None) -> None:
+        """Print a string representation of this quantity.
+
+        Parameters
+        ----------
+        selection : str | None
+            Select which source of the quantity is printed. If you select multiple
+            sources, py4vasp prints one block per source.
+        """
+        print(self.__str__(selection))
+
     def __str__(self, selection: str | None = None) -> str:
         return merge_strings(
             self._source,
